949f34d86f6bf58c311a145e03917cbe0365c47b
alisyasyafiq <35653045+alisyasyafiq@users.noreply.github.com>
2018-01-21 18:39:03 +0800

    Update Lab-Exercise-4.cpp

--- Lab-Exercise-4.cpp
+++ Lab-Exercise-4.cpp
@@ -3,22 +3,23 @@
 
 
 #include <iostream>
+using namespace std;
 
 
 int main()
 {
+      int i,j,rows;
 	cout<<"Display the pattern"<<endl;
 	cout<<"-------------------"<<endl;
 
-	i,j,rows;
-	cout<<"Input number of rows :"<<endl;
+	cout<<"Input number of rows: "<<endl;
+	cin >> rows;
 
-
-	for(i=1;i<=rows;i++)
+        for(i=1;i<=rows;i++)
 	{
 	for(j=1;j<=i;j++)
-		
-	cout<<endl;
+        cout<<i;
+        cout<<endl;
 	}
 }
 	
